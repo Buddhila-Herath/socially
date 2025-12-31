ae0313cc80e6262342b7770807c941f2504221f0
Buddhila-Herath <buddhilaherath32620@gmail.com>
2025-12-31 16:49:47 +0530

    s

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal" />
+        <Button>Sign In</Button>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
+      <Button variant={"secondary"}>Click me</Button>
+    </div>
+  );
+}
