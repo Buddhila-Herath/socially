c70767364946d601a86dfa6fc2433e15881905a7
Buddhila-Herath <buddhilaherath32620@gmail.com>
2026-01-02 11:07:26 +0530

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>home page</h1>
