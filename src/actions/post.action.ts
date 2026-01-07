5303547f5918b18504b80347e00d2f0215942887
Buddhila-Herath <buddhilaherath32620@gmail.com>
2026-01-07 11:47:20 +0530

    added content

---
+++ src/actions/post.action.ts
@@ -0,0 +1,27 @@
+"use server"
+
+import prisma from "@/lib/Prisma";
+import { getDbUserId } from "./user.action";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(content: string, image: string) {
+    try {
+        const userId = await getDbUserId();
+        if (!userId) {
+            return { success: false, error: "User not authenticated" };
+        }
+        const post = await prisma.post.create({
+            data: {
+                content,
+                image,
+                authorId: userId,
+            }
+        })
+
+        revalidatePath("/");
+        return { success: true, post };
+    } catch (error) {
+        console.log("Error creating post", error);
+        return { success: false, error: "Error creating post" };
+    }
+}
